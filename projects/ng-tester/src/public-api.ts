08c277f187a09d783d18d1d54864d69b071b42d4
Brendan Sawyer <brendan.sawyer@excella.com>
2020-02-22 18:06:09 -0500

    Update public-api.ts

--- projects/ng-tester/src/public-api.ts
+++ projects/ng-tester/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of ng-tester
  */
 
-export * from './lib/ng-tester.service'
-export * from './lib/ng-tester.component'
-export * from './lib/ng-tester.module'
+// export * from './lib/ng-tester.service'
+// export * from './lib/ng-tester.component'
+// export * from './lib/ng-tester.module'
